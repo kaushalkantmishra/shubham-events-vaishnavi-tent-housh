c3867ec586249d29dd2e406400ac088247583d6a
kaushalkantmishra <kaushalkantmishra127@gmail.com>
2024-08-05 22:20:06 +0530

    added contact us form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@
 import React, { FormEvent, useState } from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
-function MusicSchoolContactUs() {
+function ContactUs() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
@@ -14,21 +14,15 @@ function MusicSchoolContactUs() {
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
-      {" "}
-      {/* Ensure the container is relative */}
-      {/* BackgroundBeams with adjusted z-index */}
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
-      {/* Content with higher z-index */}
       <div className="max-w-2xl mx-auto p-4 relative z-10">
-        {" "}
-        {/* Add relative and z-10 to bring content to the front */}
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
-          We&apos;re here to help with any questions about our courses,
-          programs, or events. Reach out and let us know how we can assist you
-          in your musical journey.
+          We&apos;re here to assist you with any questions or inquiries about
+          our event planning, catering services, or consultations. Reach out to
+          us, and let&apos;s make your event unforgettable!
         </p>
         <form onSubmit={handleSubmit} className="space-y-4 mt-4">
           <input
@@ -59,4 +53,4 @@ function MusicSchoolContactUs() {
   );
 }
 
-export default MusicSchoolContactUs;
+export default ContactUs;
